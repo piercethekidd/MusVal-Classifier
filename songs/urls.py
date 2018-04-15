0772c2c14700cd18074a33f89fbba175a85677e0
slobaddik <kiddevangelista@gmail.com>
2018-04-15 17:04:31 +0800

    Added routes in urls.py

--- songs/urls.py
+++ songs/urls.py
@@ -7,4 +7,9 @@ app_name = 'songs'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('display/', views.display, name="display"),
+	path('results/', views.results, name="results"),
+	path('statistics/', views.statistics, name="statistics"),
+	path('methodology/', views.methodology, name="methodology"),
+	path('search/', views.search, name="search"),
+
 ]
--- songs/views.py
+++ songs/views.py
@@ -5,13 +5,35 @@ from songs.models import Song
 
 
 # Create your views here.
+
+# Index
 def index(request):
 	wat = "HELLO! WORLD"
 	context = {'wat': wat}
 	return render(request, 'songs/index.html', context)
 
 
-def display (request):
+def display(request):
 	song_list = Song.objects.order_by('id')[:1100]
 	context = {'song_list': song_list,}
 	return render(request, 'songs/display.html', context)
+
+# Results
+def results(request):
+	return render(request, 'songs/results.html', {})
+
+# Statistics
+def statistics(request):
+	return render(request, 'songs/statistics.html', {})
+
+# Methodology
+def methodology(request):
+	return render(request, 'songs/methodology.html', {})
+
+# Search
+def search(request):
+	return render(request, 'songs/search.html', {})
+
+# Classify
+def classify(request):
+	return render(request, 'songs/classify.html', {})
